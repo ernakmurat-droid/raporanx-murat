9f160435fdbbec303e00c045ec8704a69f66f816
ernakmurat-droid <ernakmurat@gmail.com>
2026-01-13 17:40:30 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,28 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCMwx6S6rq1lnNJuHOxc38ij3qU4ankSxs",
-  authDomain: "raporanx.firebaseapp.com",
-  projectId: "raporanx",
-  storageBucket: "raporanx.firebasestorage.app",
-  messagingSenderId: "715194328346",
-  appId: "1:715194328346:web:46f0bb98941e01ead6f8a3"
-};
+// firebase-config.js (TEK KAYNAK - tüm sayfalar buradan kullanacak)
+(function () {
+  const FB_CFG = {
+    apiKey: "AIzaSyCMwx6S6rq1lnNJuHOxc38ij3qU4ankSxs",
+    authDomain: "raporanx.firebaseapp.com",
+    projectId: "raporanx",
+    storageBucket: "raporanx.firebasestorage.app",
+    messagingSenderId: "715194328346",
+    appId: "1:715194328346:web:46f0bb98941e01ead6f8a3"
+  };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+  // Firebase zaten başladıysa tekrar başlatma
+  if (!firebase.apps.length) {
+    firebase.initializeApp(FB_CFG);
+  }
 
-const auth = firebase.auth();
-const db = firebase.firestore();
+  // GLOBAL olarak dışarı ver (tüm sayfalar buradan kullanacak)
+  window.auth = firebase.auth();
+  window.db = firebase.firestore();
+  window.FB_CFG = FB_CFG;
 
-window.auth = auth;
-window.db = db;
-
-// Oturum kalıcı
-auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL).catch(() => {});
+  // İstersen bu guard'ı KALDIR (bazı sayfalarda saçma yönlendirme yapabiliyor)
+  // auth.onAuthStateChanged((user) => {
+  //   if (!user && !window.location.href.includes('index.html')) {
+  //     window.location.href = 'index.html';
+  //   }
+  // });
+})();
