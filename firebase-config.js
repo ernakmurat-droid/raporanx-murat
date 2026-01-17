0c82f36170c20ce7903e89f6b4b121bc7ba37eb0
ernakmurat-droid <ernakmurat@gmail.com>
2026-01-17 22:04:00 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "XXXX",
-  authDomain: "XXXX.firebaseapp.com",
-  projectId: "XXXX",
-  storageBucket: "XXXX.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "SENİN_API_KEY",
+  authDomain: "projen.firebaseapp.com",
+  projectId: "projen",
+  storageBucket: "projen.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef123456"
 };
 
 firebase.initializeApp(firebaseConfig);
