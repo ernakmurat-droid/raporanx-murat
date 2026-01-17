ad4e9abc1843fe9a158455b2ce10ae237de95c19
ernakmurat-droid <ernakmurat@gmail.com>
2026-01-17 23:33:26 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,23 @@
-const firebaseConfig = {
-  apiKey: "SENİN_API_KEY",
-  authDomain: "projen.firebaseapp.com",
-  projectId: "projen",
-  storageBucket: "projen.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
-};
+// firebase-config.js (TEK KAYNAK - tüm sayfalar buradan kullanacak)
+(function () {
+  const FB_CFG = {
+    apiKey: "AIzaSyCMwx6S6rq1lnNJuHOxc38ij3qU4ankSxs",
+    authDomain: "raporanx.firebaseapp.com",
+    projectId: "raporanx",
+    storageBucket: "raporanx.firebasestorage.app",
+    messagingSenderId: "715194328346",
+    appId: "1:715194328346:web:46f0bb98941e01ead6f8a3",
+    measurementId: "G-V749P1HB80"  // Analytics için, zararsız
+  };
 
-firebase.initializeApp(firebaseConfig);
+  // Firebase zaten başladıysa tekrar başlatma
+  if (!firebase.apps.length) {
+    firebase.initializeApp(FB_CFG);
+  }
+
+  // GLOBAL olarak dışarı ver (tüm sayfalar buradan kullanacak)
+  window.auth = firebase.auth();
+  window.db = firebase.firestore();
+  window.FB_CFG = FB_CFG;
+
+})();
